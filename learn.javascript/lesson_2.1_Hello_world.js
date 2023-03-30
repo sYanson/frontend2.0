0343659171932d80aac93a13a996851ba5f16fe4
sYanson <yanson_sergey@icloud.com>
2023-03-30 22:04:19 +0700

    My commit

--- learn.javascript/lesson_2.1_Hello_world.js
+++ learn.javascript/lesson_2.1_Hello_world.js
@@ -1,14 +1,11 @@
 // Создайте страницу, которая отобразит сообщение «Я JavaScript!».
 
-<html>
     <script>
     alert ('Я JavaScript!');
     </script>
-</html>
 
 // Возьмите решение предыдущей задачи Вызвать alert, и измените его.
 // Извлеките содержимое скрипта во внешний файл alert.js, лежащий в той же папке.
+ 
 
-<html>
-    <script src ="./alert.js"></script>
-</html>
+// <script src ="./alert.js"></script>
---
+++ learn.javascript/lesson_2.6_Interaction.js
@@ -0,0 +1,8 @@
+//alert, prompt, confirm
+//Создайте страницу, которая спрашивает имя у пользователя и выводит его.
+
+
+    <script>
+    let name = prompt("What's your name?"); //ask their name
+        alert (`Hello, ${name}!`); //say Hello! to them
+    </script>
