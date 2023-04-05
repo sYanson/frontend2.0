57b8457bdb50c3fcb97e332b02482bd1e6385931
sYanson <yanson_sergey@icloud.com>
2023-04-05 22:57:43 +0700

    My commit

--- learn.javascript/lesson_4.1_Object.js
+++ learn.javascript/lesson_4.1_Object.js
@@ -18,3 +18,52 @@ schedule["8:30"] = "get up";
 
 alert( isEmpty(schedule) ); // false
 
+// Мое решение:
+
+let schedule = {}; // у нас есть объект schedule
+
+function = isEmpty(obj) {
+    for (let key in obj) { //перебираем возможные ключи
+        if key === undefined { // если ключи не найдены, должно вернуться undefined
+            alert( isEmpty(obj) ); // true
+}       else {
+            alert( isEmpty(obj) ); // false
+}
+}
+}
+
+// Решение учебника:
+
+function isEmpty(obj) {
+    for (let key in obj) {
+      // если тело цикла начнет выполняться - значит в объекте есть свойства
+      return false;
+    }
+    return true;
+  }
+
+// 3. Можно ли изменить объект, объявленный с помощью const? Как вы думаете?
+
+const user = {
+    name: "John"
+  };
+  
+  // это будет работать?
+  user.name = "Pete";
+  // содержимое объекта может быть изменено, значение name изменится c John на Pete:
+  
+const user = {
+    name: "Pete"
+  };
+
+// 4. У нас есть объект, в котором хранятся зарплаты нашей команды:
+
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+  }
+// Напишите код для суммирования всех зарплат и сохраните результат в переменной sum. Должно получиться 390.
+// Если объект salaries пуст, то результат должен быть 0.
+
+let result = (let (for key in obj)) ? sum[key] : 0 ;
