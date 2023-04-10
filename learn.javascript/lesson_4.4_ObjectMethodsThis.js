de0696225534fcce1c283b25ba9c57fb46f3c78d
sYanson <yanson_sergey@icloud.com>
2023-04-11 01:23:02 +0700

    ladder is done

--- learn.javascript/lesson_4.4_ObjectMethodsThis.js
+++ learn.javascript/lesson_4.4_ObjectMethodsThis.js
@@ -13,7 +13,7 @@ function makeUser() {
 let user = makeUser(); // user копирует объект, который вызывает makeUser()
 
 alert( user.ref.name ); // Каким будет результат?
-// undefined, this в этом случае неопределен
+// undefined, this в этом случае неопределен?
 
 //////////////////////////////////////////////////////////////////////////////
 
@@ -35,7 +35,7 @@ let calculator = {
   mul() {
     return (this.a * this.b);
   }
-}; // работает!
+};
 
 /////////////////////////////////////////////////////////////////////////////////////////
 
@@ -64,20 +64,21 @@ ladder.down();
 ladder.showStep(); // 0
 
 // Измените код методов up, down и showStep таким образом, чтобы их вызов можно было сделать по цепочке, например так:
-
 ladder.up().up().down().showStep().down().showStep(); // показывает 1 затем 0
-
 // Такой подход широко используется в библиотеках JavaScript.
 
 let ladder = {
   step: 0,
   up() {
     this.step++;
+    return this; // возвращаем объект с новым step
   },
   down() {
     this.step--;
+    return this; // 
   },
-  showStep: function() { // показывает текущую ступеньку
+  showStep: function() {
     alert( this.step );
+    return this; //
   }
 };
