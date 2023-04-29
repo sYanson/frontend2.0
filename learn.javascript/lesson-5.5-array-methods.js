1a998e83db2d6b58014c268ef66578869d5c92cd
sYanson <yanson_sergey@icloud.com>
2023-04-29 23:43:22 +0700

    added calculator

--- learn.javascript/lesson-5.5-array-methods.js
+++ learn.javascript/lesson-5.5-array-methods.js
@@ -130,8 +130,7 @@ powerCalc.addMethod("*", (a, b) => a * b);
 powerCalc.addMethod("/", (a, b) => a / b);
 powerCalc.addMethod("**", (a, b) => a ** b);
 
-let result = powerCalc.calculate("2 ** 3");
-alert( result ); // 8
+let result = powerCalc.calculate("2 ** 3"); // 8
 
 // Для этой задачи не нужны скобки или сложные выражения.
 // Числа и оператор разделены ровно одним пробелом.
@@ -139,8 +138,25 @@ alert( result ); // 8
 
 //
 function Calculator() {
-    this.calculate: function(str) {
-  }
+  this.methods = {
+  "-": (a, b) => a - b,
+  "+": (a, b) => a + b
+};
+
+  this.calculate = function(str) {
+    let split = str.split(' '),
+      a = +split[0],
+      op = split[1],
+      b = +split[2]
+      if (!this.methods[op] || isNaN(a) || isNaN(b)) {
+        return NaN;
+  } return this.methods[op](a, b);
+}
+
+  this.addMethod = function(name, func) {
+    this.methods[name] = func;
+  };
+}
 
   /////////////////////////////////////////////////////////////////////////////
 
